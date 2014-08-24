1111ef4988419ba53e37e431b6324444941aeba8
schiopelu <tbodica@gmail.com>
2014-08-24 18:55:36 +0300

    Revert "Added dice.hpp header and tested rolls"
    This reverts commit 5f0e8db78648a5e6ffb1c219fb0d361664d44cf1.

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,5 @@
 #include <iostream>
-#include <fstream>
-#include "dice.hpp"
-
-using namespace table;
-using namespace std;
-
-int mat[8][8]; // matrice de raspandire
-
-ofstream g("dicerolltest.out");
 
 int main(void) {
-    //std::cout << "Primul commit la proiectul table";
-
-    for (int i = 0; i < 1000000; ++i) // 1 milion de aruncari cu 2 zaruri, 23 secunde pe AMD PHENOM 2 X4 3.2 Ghz
-    {
-        pair<int, int> p;
-        p = DoubleDiceRoll();
-
-        mat[p.first][p.second]++;
-        mat[p.second][p.first]++;
-    }
-
-    g << '\n';
-
-    for (int i = 1; i <= 6; ++i) // afisare matrice raspandire
-    {
-        for (int j = 1; j <= 6; ++j)
-            g << mat[i][j] << ' ';
-        g << '\n';
-    }
-
-    g.close();
+    std::cout << "Primul commit la proiectul table";
 }
