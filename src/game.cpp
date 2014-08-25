ed8be2ab0bc3992600ff440bd75c1167b62af8b9
schiopelu <tbodica@gmail.com>
2014-08-26 01:20:50 +0300

    Added operator=, copy constructor and void constructor

--- src/game.cpp
+++ src/game.cpp
@@ -62,20 +62,72 @@ namespace table {
         return !(a.color == b.color && a.number == b.number);
     }
 
-
     Board::Board(void) : points(),
                          current_player(),
-                         current_dice(),
                          done(),
                          out(),
+                         current_dice(),
                          starting_phase(),
                          is_game_won(),
                          remaining_moves()
     {
-        points[0] = points[NUM_POINTS-1] = 2;
-        points[5] = points[NUM_POINTS-6] = 5;
-        points[7] = points[NUM_POINTS-8] = 3;
-        points[12] = points[NUM_POINTS-13] = 5;
+        points[0] = Point(Color::WHITE, 2), points[NUM_POINTS-1] = Point(Color::BLACK, 2);
+        points[5] = Point(Color::WHITE, 5), points[NUM_POINTS-6] = Point(Color::BLACK, 5);
+        points[7] = Point(Color::WHITE, 3), points[NUM_POINTS-8] = Point(Color::BLACK, 3);
+        points[12]= Point(Color::WHITE, 5), points[NUM_POINTS-13]= Point(Color::BLACK, 5);
+    }
 
+    Board::Board(const Board& board) {
+
+        int c = 0;
+        for (const auto i : board.points )
+            this->points[c++] = i;
+
+        this->current_player = board.current_player;
+
+        c = 0;
+        for ( const auto i : board.done )
+            this->done[c++] = i;
+
+        c = 0;
+        for ( const auto i : board.out )
+            this->out[c++] = i;
+
+        this->current_dice = board.current_dice;
+        this->starting_phase = board.starting_phase;
+        this->is_game_won = board.is_game_won;
+
+        c = 0;
+        for ( auto i : board.remaining_moves )
+            this->remaining_moves[c++] = i;
+    }
+
+    Board& Board::operator=(const Board& board) {
+
+                         // check for assignment to self
+        if ( this != &board ) {
+            int c = 0;
+            for (const auto i : board.points )
+                this->points[c++] = i;
+
+            this->current_player = board.current_player;
+
+            c = 0;
+            for ( const auto i : board.done )
+                this->done[c++] = i;
+
+            c = 0;
+            for ( const auto i : board.out )
+                this->out[c++] = i;
+
+            this->current_dice = board.current_dice;
+            this->starting_phase = board.starting_phase;
+            this->is_game_won = board.is_game_won;
+
+            c = 0;
+            for ( auto i : board.remaining_moves )
+                this->remaining_moves[c++] = i;
+        }
+        return *this;
     }
 }
