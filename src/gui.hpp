46f73583fe99b1752f0a158a1572e2cc22493774
Eric Rübl <ericptcolegi@yahoo.com>
2014-08-26 01:21:13 +0300

    Inceput Al dente

--- src/gui.hpp
+++ src/gui.hpp
@@ -1,10 +1,24 @@
 #ifndef _GUI_
 #define _GUI_
 
+#include <iostream>
+
 namespace table
 {
-	
-
+	class gui
+	{
+	private:
+		char draw[44][20];
+	public:
+		void show
+		{
+			  for (int i = 0; i <= 20; ++i)
+				for (int j = 0; j <= 44)
+					std::cout << draw[i][j];
+				  
+		}
+		
+	};
 }
 
 
