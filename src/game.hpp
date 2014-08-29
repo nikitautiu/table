b14c83dbd22a54447e4fa7665cd8feb662d1e04b
Eric Rübl <ericptcolegi@yahoo.com>
2014-08-29 22:43:00 +0300

    da

---
+++ src/game.hpp
@@ -0,0 +1,171 @@
+#ifndef game_hpp_guard
+#define game_hpp_guard
+
+#include <cstdint>
+#include <array>
+#include <vector>
+#include <set>
+#include <utility>
+#include <exception>
+
+namespace table {
+/******************************************************************************
+                        POINT CLASS
+*******************************************************************************/
+    enum class Color : int8_t {
+        // tipurile de table/jucatori
+        WHITE = -1, BLACK = 1
+    };
+
+    struct Point {
+        Color color;
+        int number;
+
+        Point(void);
+        Point(Color, int);
+        Point(const Point&);
+        Point& operator=(Point);
+
+        Point& operator+=(int);
+        Point& operator-=(int);
+        Point& operator++(void);
+        Point operator++(int);
+        Point& operator--(void);
+        Point operator--(int);
+    };
+
+    // operatori non-member non-friend
+    const Point operator+(Point, int); // audauga un nr de puluri
+    const Point operator-(Point, int);
+    bool operator==(Point, Point); // operatori de compararare
+    bool operator!=(Point, Point);
+
+
+/******************************************************************************
+                            DEFINES AND ENUMS
+*******************************************************************************/
+    const int NUM_POINTS = 24;
+    const int NUM_CHECKERS_PER_PLAYER = 15;
+
+    using CheckerMove = std::pair <int, int>;
+    using Turn = std::vector <CheckerMove>;
+    using PointArray = std::array <Point, NUM_POINTS>;
+    using DicePair = std::pair <int, int>;
+
+    enum class GamePhase : int8_t {
+        STARTING, GAME, NORMAL_WIN, TECHNICAL_WIN
+    };
+
+
+/******************************************************************************
+                            DEFINES AND ENUMS
+*******************************************************************************/
+struct BoardState {
+    PointArray points;
+    std::array <unsigned int, 2> done;
+    std::array <unsigned int, 2> out;
+
+    void initialize (void) {
+        points[0]  = Point(Color::WHITE, 2), points[NUM_POINTS-1]  = Point(Color::BLACK, 2);
+        points[5]  = Point(Color::WHITE, 5), points[NUM_POINTS-6]  = Point(Color::BLACK, 5);
+        points[7]  = Point(Color::WHITE, 3), points[NUM_POINTS-8]  = Point(Color::BLACK, 3);
+        points[12] = Point(Color::WHITE, 5), points[NUM_POINTS-13] = Point(Color::BLACK, 5);
+    }
+
+    int get_out(Color color) const {
+        // primeste culoare si returneaza cate scoase de culoara aia is
+        int poz = (color ==  Color::WHITE ? 0 : 1);
+        return out[poz];
+    }
+
+    int get_done(Color color) const {
+        // primeste culoare si returneaza cate de culoarea aia o terminat
+        int poz = (color ==  Color::WHITE ? 0 : 1);
+        return done[poz];
+    }
+
+    void set_out(Color color, int new_value) {
+        int poz = (color ==  Color::WHITE ? 0 : 1);
+        out[poz] =  new_value;
+    }
+
+    void set_done(Color color, int new_value) {
+        int poz = (color ==  Color::WHITE ? 0 : 1);
+        done[poz] = new_value;
+    }
+
+
+};
+
+/******************************************************************************
+                        BACKGAMMON CLASS
+*******************************************************************************/
+    class Backgammon {
+    private:
+        BoardState current_board_state;
+        Color current_player;
+        Color winner;
+        GamePhase current_phase;
+        DicePair current_dice;
+    public:
+        Backgammon(void); // constructor inplicit
+        Backgammon(const Backgammon&); // constructor copiere
+        Backgammon& operator=(Backgammon); // operator copiere
+
+        void initialize(void) {
+            (*this) = Backgammon(); // reinitializeaza cu nul
+            current_phase = GamePhase::STARTING;
+            current_board_state.initialize();
+        }
+
+        Color get_winner(void) const {
+            // daca ii gata partida, returneaza castigatorul
+            if(current_phase != GamePhase::NORMAL_WIN && current_phase != GamePhase::TECHNICAL_WIN)
+                throw std::runtime_error("No winner yet"); // daca nu, exceptie
+            return winner;
+        }
+
+        Color get_current_player(void) const {
+            return current_player; // returneaza playerul actual
+        }
+
+        GamePhase get_phase(void) const {
+            return current_phase;
+        }
+
+        BoardState get_board_state(void) const {
+            // returneaza reprezentarea tablei
+            return current_board_state;
+        }
+
+        DicePair get_dice(void) const {
+            return current_dice;
+        }
+
+        void roll_dice(void); // arunca cu zarul, actualizeaza current_dice(atentie cand current_game_phase = STARTING)
+        void submit_turn(Turn); // trimite un set de mutari. daca sunt ilegale arunca exceptie. daca nu, efectueaza
+    };
+
+
+/******************************************************************************
+                        HELPERBOARD CLASS
+*******************************************************************************/
+    class HelperBoard {
+    public:
+        // clasa pe care poti face mutari, da undo, verifica legalitatea lor etc
+        HelperBoard(void);
+        HelperBoard(const Backgammon&); // initializeaza o tabla temporara din clasa de joc
+        HelperBoard(const HelperBoard&); // constructor copiere
+        HelperBoard& operator=(const HelperBoard&); // operator copiere
+
+        Turn get_turn(void) const; // returneaza mutarile de pana acuma
+        void push_move(CheckerMove); // face o mutare. daca ilegala, arunca exceptie
+        void pop_move(void); // da undo la ultima mutare
+
+        std::set <CheckerMove> get_immediately_legal_moves(void) const; // returneaza mutarile de cate un pul imediat legale(prima data cele obligatorii)
+        std::multiset <int> get_remaining_moves(void) const; // returneaza mutarile ramase
+        bool is_turn_done(void) const; // verifica daca s-a facut o tura completa legala
+    };
+}
+
+#endif
--- src/gui.hpp
+++ src/gui.hpp
@@ -2,47 +2,167 @@
 #define _GUI_
 
 #include <iostream>
+#include <cstdint>
+#include <array>
+#include <vector>
+#include <utility>
+#include "game.hpp"
 
 namespace table
-{
-	class gui
+{	
+	class Gui
 	{
 	private:
-		char draw[44][20];
-	public:
-		void show()
+		char m[45][21];
+		int xz;
+		int dxd;
+		int dxu;
+		std::multiset <int> zario;
+		std::array <Point, NUM_POINTS> poz;
+
+		void fillup(int dx, Point pt) // metoda umplere punct de pe interfata
 		{
-			  for (int i = 0; i <= 20; ++i)
-				for (int j = 0; j <= 44)
-					std::cout << draw[i][j];
-				  
-	}
-		void droll()
-		{
-
-		}
-
-		void points()
-		{
-
-		}
-
-		void turn()
-		{
-
-		}
-
-		void score()
-		{
-
-		}
-
-		void margin()
-		{
-			for (int i = 3; i <= 17; ++i)
+			char pul;
+			if (pt.color == Color::BLACK)
+				pul = '@';
+			else
+				pul = 'O';
+			int t = 4 + pt.number;
+			if (pt.number > 5)
 			{
-				draw
+				t = 4;
+				m[dx][8] = (pt.number - 5);
 			}
+			for (int i = 4; i < t; ++i)
+				m[dx][i] = pul;
+		}
+
+		void filldown(int dx, Point pt) // metoda umplere punct de pe interfata
+		{
+			char pul;
+			if (pt.color == Color::BLACK)
+				pul = '@';
+			else
+				pul = 'O';
+			int t = 4 + pt.number;
+			if (pt.number > 5)
+			{
+				t = 4;
+				m[dx][12] = (pt.number - 5);
+			}
+			for (int i = 16; i > t; --i)
+				m[dx][i] = pul;
+		}
+
+	public:
+		Gui(void); //constructor default
+
+		void draw(void) //afisare matrice pe consola
+		{
+			system("cls");
+			for (int i = 0; i < 21; ++i)
+			{
+				for (int j = 0; j < 45; ++j)
+					std::cout << m[j][i];
+				std::cout << '\n';
+			}
+		}
+
+		void margin(void) //margine interfata si desenare tabla
+		{
+			for (int i = 0; i < 45; ++i) //orizontal
+			{
+				m[i][0] = '-';
+				m[i][20] = '-';
+
+				if (i >= 3 && i <= 33)
+				{
+					m[i][3] = '-';
+					m[i][18] = '-';
+				}
+			}
+
+			for (int i = 0; i < 21; ++i) //vertical
+			{
+				m[0][i] = '|';
+				m[44][i] = '|';
+
+				if (i >= 4 && i <= 17)
+				{
+					m[3][i] = '|';
+					m[17][i] = '|';
+					m[19][i] = '|';
+					m[33][i] = '|';
+				}
+
+			}
+
+		}
+
+		void game(HelperBoard temp) //tabla si cursor
+		{
+			BoardState bs = temp.get_board_state();
+			poz = bs.points; //tabla
+
+			dxd = 5;
+			dxu = 31;
+
+			for (int i = 0; i < 6; ++i)
+			{
+				filldown(dxd, poz[i]);
+				dxd += 2;
+			}
+
+			dxd = 21;
+
+			for (int i = 6; i < 12; ++i)
+			{
+				filldown(dxd, poz[i]);
+				dxd += 2;
+			}
+
+			for (int i = 12; i < 18; ++i)
+			{
+				fillup(dxu, poz[i]);
+				dxu -= 2;
+			}
+
+			dxu = 15;
+
+			for (int i = 18; i < 24; ++i)
+			{
+				fillup(dxu, poz[i]);
+				dxu -= 2;
+			}
+			
+			m[18][6] = bs.get_done(Color::BLACK); //iesite
+			m[18][12] = bs.get_done(Color::WHITE);
+			m[18][8] = bs.get_out(Color::BLACK); //scoase
+			m[18][10] = bs.get_out(Color::WHITE);
+		}
+
+		void update(Backgammon temp) // zaruri ,scoase, iesite si mutari posibile
+		{
+
+			
+
+			HelperBoard hb = HelperBoard(temp); //zaruri
+			zario = hb.get_remaining_moves();
+			xz = 35;
+			for (int i : zario)
+			{
+				m[i][10] = i;
+				xz += 2;
+			}
+
+			
+
+
+		}
+
+		void info(Backgammon temp) //scor, rand, text, stare joc
+		{
+			
 		}
 	};
 }
