01b99b70980765f96fc7c7c1b5ba781a05f08f04
vitiv <nikita.utiu@gmail.com>
2014-09-15 22:12:00 +0300

    implementat partial regullile romanesti

--- src/variants/rules_ro.cpp
+++ src/variants/rules_ro.cpp
@@ -4,21 +4,70 @@
 
 namespace table
 {
+    namespace internal
+    {
+        RoDiceHelper::RoDiceHelper(Color player)
+        {
+
+        }
+
+        void RoDiceHelper::give_dice(DicePair)
+        {
+
+        }
+
+        bool RoDiceHelper::is_done(void) const
+        {
+
+        }
+
+        Color RoDiceHelper::get_winner(void) const
+        {
+
+        }
+
+        std::pair<bool, DicePair> RoDiceHelper::get_double_pair(void) const
+        {
+
+        }
+    }
+
 /*******************************************************************************
                             CLASA ROMATCH
 *******************************************************************************/
-    RoMatch::RoMatch(int rounds_to_win=3, bool with_technical=true) :
+    RoMatch::RoMatch(int rounds_to_win, bool with_technical) :
         IMatch(),
         _num_rounds_to_win(rounds_to_win),
-        _with_technical(with_technical)
+        _with_technical(with_technical),
+        _opening_roll_phase(new RoOpeningRollPhase(Color::WHITE)),
+        _game_phase()
     {
-        _current_phase.reset(new RoOpeningRollPhase(Color::WHITE)); // noua faza de joc
+        _current_phase = _opening_roll_phase.get();
     }
+
 
     void RoMatch::next_phase(void)
     {
         IMatch::next_phase(); // se cheama implementare implicita
-        // de implementat restu
+        // daca a trecut de pasul anterior inseamna ca se poate trece la urmatoarea faza
+
+
+        auto starting_player = (_current_phase->get_win_outcome()).second;
+        if(_current_phase->get_phase_type() == PhaseType::OPENING_ROLL)
+        {
+            auto win_type = (_current_phase->get_win_outcome()).first;
+            if(win_type == "double")
+            {
+                auto starting_pair = _opening_roll_phase->get_starting_dices();
+                _game_phase.reset(new RoGamePhase(starting_player, starting_pair)); // cu dubla de inceput
+            }
+            else
+                _game_phase.reset(new RoGamePhase(starting_player)); // fara dubla de inceput
+            _opening_roll_phase.reset();
+        }
+        else
+            _game_phase.reset(new RoGamePhase(starting_player)); // se incepe cu castigatorul
+        _current_phase = static_cast<IPhase*> (_game_phase.get()); // type-safe cast
     }
 
     void RoMatch::on_phase_action(void)
@@ -29,7 +78,9 @@ namespace table
 /*******************************************************************************
                             CLASA ROOPENINGROLLPHASE
 *******************************************************************************/
-    RoOpeningRollPhase::RoOpeningRollPhase(Color starting_player) : IPhase()
+    RoOpeningRollPhase::RoOpeningRollPhase(Color starting_player) :
+        IPhase(),
+        _dice_helper(starting_player)
     {
         _phase_type = PhaseType::OPENING_ROLL;
         _current_player = starting_player;
@@ -38,5 +89,39 @@ namespace table
     void RoOpeningRollPhase::roll_dice(void)
     {
         IPhase::roll_dice();
+        _dice_helper.give_dice(_current_dices);
+        if(_dice_helper.is_done())
+        {
+            auto win_type_string = std::string(_dice_helper.get_double_pair().first ? "double" : "normal"); //verifica pt dubla
+            _win_outcome = std::make_pair(win_type_string, _dice_helper.get_winner());
+        }
+        _starting_dice_pair = _dice_helper.get_double_pair().second;
+
+    }
+
+    DicePair RoOpeningRollPhase::get_starting_dices(void) const
+    {
+        if(_win_outcome.first != "double")
+            throw std::runtime_error("There hasn't been a double");
+        return _starting_dice_pair;
+    }
+
+/*******************************************************************************
+                            CLASA ROGAMEPHASE
+*******************************************************************************/
+    RoGamePhase::RoGamePhase(Color starting_player, DicePair starting_double) :
+        IPhase()
+    {
+
+    }
+
+    void RoGamePhase::roll_dice(void)
+    {
+
+    }
+
+    void RoGamePhase::submit_moves(Turn moves)
+    {
+
     }
 }
--- src/variants/rules_ro.hpp
+++ src/variants/rules_ro.hpp
@@ -7,11 +7,33 @@
 
 namespace table
 {
+    // FORWARD DECLARATIONS
+    class RoOpeningRollPhase;
+    class RoGamePhase;
+
+    namespace internal
+    {
+        class RoDiceHelper
+        {
+        public:
+            // TODO: de implementat
+            RoDiceHelper(Color); // primeste playerul care incepe
+
+            void give_dice(DicePair); // da zarurile arunctae. progreseaza algoritmul
+            bool is_done(void) const; // returneaza true daca s-a castigat cineva darea cu zarul
+            Color get_winner(void) const; // returneaza castigatorul
+            std::pair<bool, DicePair> get_double_pair(void) const; // returneaza o perche de  tipul (bool e dubla?, DicePair daca da atunci dubla)
+        };
+    }
+
     class RoMatch : public IMatch
     {
     private:
         int _num_rounds_to_win;
         bool _with_technical;
+
+        std::unique_ptr<RoOpeningRollPhase> _opening_roll_phase;
+        std::unique_ptr<RoGamePhase> _game_phase;
     public:
         RoMatch(int, bool);
 
@@ -23,7 +45,7 @@ namespace table
     class RoGamePhase : public IPhase
     {
     public:
-        RoGamePhase(Color, DicePair); // creeaza o linie cu un player care incepe si un posibil zar de inceput
+        RoGamePhase(Color, DicePair=DicePair(0, 0)); // creeaza o linie cu un player care incepe si un posibil zar de inceput
 
         virtual void roll_dice(void) override;
         virtual void submit_moves(Turn) override;
@@ -31,9 +53,15 @@ namespace table
 
     class RoOpeningRollPhase : public IPhase
     {
+        // faza de inceput pt jtablele romanesti
+        // are valorile de castig "not_won", "double" sau "normal"
+    private:
+        internal::RoDiceHelper _dice_helper;
+        DicePair _starting_dice_pair;
     public:
         RoOpeningRollPhase(Color); // creaza o faza de inceput cu un player care sa arunce primul
 
+        DicePair get_starting_dices(void) const;
         virtual void roll_dice(void) override;
     };
 }
